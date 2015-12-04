d239f89d2beda937f34fe0908a298670d47fe942
Peyton Caseria <PeytonCaseria@gmail.com>
2015-12-03 17:15:28 -0700

    Fixed File Typo, Updated ToA.rb Initial setup for random content generating throughout arrays inside arrays next is irb testing and automated testing with Rspec. Rspec block goal is to check if the ToA array has AA - ZZ nested inside and then pull the full name for the user rather than the abbreviation, still looking into attaching the html / css / js file as an interactive Table of Alignment sitemap, by pulling the correlating html id with the css to make a gamble feeling of which file to open, should organize links with color # ids to add a cool effect on both pageland and page user actions / responsive functionality aswell. Unsure about using this on mobile devices. Seems likes a stretch with this version, look into making a phone app version because of mobile functionality limits,

---
+++ ToA.rb
@@ -0,0 +1,61 @@
+/##
+
+[ToA] ["Tomes of Alignment"]
+[ToA] [[AA]] + [BB] + [[CC]] + [[DD]] + [[EE]] + [[FF]] + [[GG]] + [[HH]] + [[II]] + [[JJ]] + [[KK]] + [[LL]] + [[MM]] + [[NN]] + [[OO]] + [[PP]] + [[QQ]] + [[RR]] + [[SS]] + [[TT]] + [[UU]] + [[V]] + [[WW]] + [[XX]] + [[YY]] + [[ZZ]]
+[AA] [" A.A. "]
+[BB] [" B.B. "]
+[CC] [" C.C. "]
+[DD] [" D.D. "]
+[EE] [" E.E. "]
+[FF] [" F.F. "]
+[GG] [" G.G. "]
+[HH] [" H.H. "]
+[II] [" I.I. "]
+[JJ] [" J.J. "]
+[KK] [" K.K. "]
+[LL] [" L.L. "]
+[MM] [" M.M. "]
+[NN] [" N.N. "]
+[OO] [" O.O. "]
+[PP] [" P.P. "]
+[QQ] [" Q.Q. "]
+[RR] [" R.R. "]
+[SS] [" S.S. "]
+[TT] [" T.T. "]
+[UU] [" U.U. "]
+[VV] [" V.V. "]
+[WW] [" W.W. "]
+[XX] [" X.X. "]
+[YY] [" Y.Y. "]
+[ZZ] [" Z.Z. "]
+/
+
+# Tough Decisions to make, 
+# Option 1 = Use Extant_Exception as the Overflow of words that are NOT Abbreviation Aligned ex. A.A.
+# Option 2 = Make a seperate Misc File that Holds the Overflow NOT A.A. (Abbreviation Aligned) ex. Old_Trends
+AA = "Alluring_Aura"
+BB = "Bright_Beacon"
+CC = "Cosmic_Concoction"
+DD = "Dim_Dawn"
+EE = "Elemental_Enchantment"
+FF = "Frequent_Form"
+GG = "Golden_Grove"
+HH = "Hollow_Hum"
+II = "Invisioning_Impact"
+JJ = "Jades_in_Jungles"
+KK = "Keen_Kindness"
+LL = "Lucid_Levitation"
+MM = "Mellow_Mantra"
+NN = "New_Nights"
+OO = "Own-my_Omen"
+PP = "Polar_Purge"
+QQ = "Quantum_Quake"
+RR = "Relaxing_Rhythm"
+SS = "Sunshade_Sensei"
+TT = "Thickening_Transcendence" + "Tilting_Tree"
+UU = "Usual_Underdog"
+VV = "Vivid_Visions"
+WW = "Wind_on_Water"
+XX = "Extant_Exception" + ""
+YY = ""
+ZZ = ""
