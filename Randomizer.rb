64a697ab12f3acd47db34737e76b0991bfba574d
peyton Caseria <peytoncaseria@gmail.com>
2015-12-04 15:04:26 -0700

    Mobile story and Backend of wireframing for ToA.rb & css & html and or  javascript

---
+++ Randomizer.rb
@@ -0,0 +1,120 @@
+
+
+
+Mobile commit
+#story and wireframes
+
+#this will define which layer will be called on function randomizer = aa - zz + others
+
+
+def AA
+  puts 'first number'
+  first_number = gets.chomp.to_i
+  first_number
+end
+
+
+def AA
+  puts 'first number'
+  first_number = gets.chomp.to_i
+  first_number
+end
+def AA
+  puts 'first number'
+  first_number = gets.chomp.to_i
+  first_number
+end
+def AA
+  puts 'first number'
+  first_number = gets.chomp.to_i
+  first_number
+end
+def AA
+  puts 'first number'
+  first_number = gets.chomp.to_i
+  first_number
+end
+def AA
+  puts 'first number'
+  first_number = gets.chomp.to_i
+  first_number
+end
+
+def AA
+  puts 'first number'
+  first_number = gets.chomp.to_i
+  first_number
+end
+
+def AA
+  puts 'first number'
+  first_number = gets.chomp.to_i
+  first_number
+end
+
+def AA
+  puts 'first number'
+  first_number = gets.chomp.to_i
+  first_number
+end
+
+def AA
+  puts 'first number'
+  first_number = gets.chomp.to_i
+  first_number
+end
+
+def AA
+  puts 'first number'
+  first_number = gets.chomp.to_i
+  first_number
+end
+
+def AA
+  puts 'first number'
+  first_number = gets.chomp.to_i
+  first_number
+end
+
+def AA
+  puts 'first number'
+  first_number = gets.chomp.to_i
+  first_number
+end
+
+
+def BB
+  puts 'bright beacon'
+  second_number = gets.chomp.to_i
+  second_number
+end
+
+def question
+puts 'Would you like to [add] or [subtract], [multiply] or [divide]?
+
+operator = gets.chomp
+
+if operator == 'add'  
+  answer = firstNumber + secondNumber
+  puts answer 
+  
+elsif operator == 'subtract'
+  
+
+  answer = firstNumber - secondNumber
+  puts answer
+
+elsif operator == 'multiply'
+
+answer = firstNumber * secondNumber
+puts answer
+
+elsif operator == 'divide'
+
+answer = firstNumber / secondNumber
+puts answer
+
+end
+end
+
+question
