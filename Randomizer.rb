7f5131f3844a25e1a48ee712118004f4247e3833
Peyton Caseria <PeytonCaseria@gmail.com>
2015-12-05 16:21:39 -0700

    Working on randomizer to grab a file and open it randomly from an array

--- Randomizer.rb
+++ Randomizer.rb
@@ -1,96 +1,152 @@
-
-
-
-Mobile commit
 #story and wireframes
-
 #this will define which layer will be called on function randomizer = aa - zz + others
 
+      def AA
+        puts 'Alluring Aura'
+        AA = gets.chomp.to_i
+        AA
+      end
 
-def AA
-  puts 'first number'
-  first_number = gets.chomp.to_i
-  first_number
+      def BB
+        puts 'Bright Beacon'
+        BB = gets.chomp.to_i
+        BB      
+      end
+
+      def CC
+        puts 'Cosmic Concoction'
+        CC = gets.chomp.to_i
+        CC
+      end
+      def DD
+        puts 'Dim Dawn'
+        DD = gets.chomp.to_i
+        DD
+      end
+      def EE
+        puts 'Elemental Enchantment'
+        EE = gets.chomp.to_i
+        EE
+      end
+      def FF
+        puts 'Frequent Form'
+        EE = gets.chomp.to_i
+        EE
+      end
+      def GG
+        puts 'Golden Grove'
+        GG = gets.chomp.to_i
+        GG  
+      end
+      def HH
+        puts 'Hollow Hum'
+        HH = gets.chomp.to_i
+        HH
+      end
+      def II
+        puts 'Invisioning Impact'
+        II = gets.chomp.to_i
+        II
+      end
+      def JJ
+        puts 'Jades in Jungles'
+        JJ = gets.chomp.to_i
+        JJ        
+      end
+      def KK
+        puts 'Keen Kindness'
+        KK = gets.chomp.to_i
+        KK
+      end
+      def LL
+        puts 'Lucid Levitation'
+        LL = gets.chomp.to_i
+        LL
+      end
+      def MM
+        puts 'Mellow Mantra'
+        MM = gets.chomp.to_i
+        MM
+      end
+      def NN
+        puts 'Number Nine'
+        NN = gets.chomp.to_i
+        NN        
+      end
+      def OO
+        puts 'Own my Omen'
+        OO = gets.chomp.to_i
+        OO
+      end
+      def PP
+        puts 'Polar Purge'
+        PP = gets.chomp.to_i
+        PP
+      end
+      def QQ
+        puts 'Quantum Quake'
+        QQ = gets.chomp.to_i 
+        QQ       
+      end
+      def RR
+        puts 'Relaxing Rhythm'
+        RR = gets.chomp.to_i
+        RR
+      end
+      def SS
+        puts 'Sunshade Sensei'
+        SS = gets.chomp.to_i
+        SS
+      end
+      def TT
+        puts 'Tilting_Tree'
+        TT = gets.chomp.to_i
+        TT
+      end
+      def UU
+        puts 'Unique Universe'
+        UU = gets.chomp.to_i
+        UU
+      end
+      def VV
+        puts 'Vivid Visions'
+        VV = gets.chomp.to_i
+        VV
+      end
+      def WW
+        puts 'Wind on Water'
+        WW = gets.chomp.to_i
+        WW
+      end
+      def XX
+        puts 'Extant Exception'
+        XX = gets.chomp.to_i
+        XX
+      end
+      def YY
+        puts 'Yuletide Yogi'
+        YY = gets.chomp.to_i
+        YY
+      end
+      def ZZ
+        puts 'Zen Zeal'
+        ZZ = gets.chomp.to_i
+        ZZ        
+      end
 end
 
 
-def AA
-  puts 'first number'
-  first_number = gets.chomp.to_i
-  first_number
-end
-def AA
-  puts 'first number'
-  first_number = gets.chomp.to_i
-  first_number
-end
-def AA
-  puts 'first number'
-  first_number = gets.chomp.to_i
-  first_number
-end
-def AA
-  puts 'first number'
-  first_number = gets.chomp.to_i
-  first_number
-end
-def AA
-  puts 'first number'
-  first_number = gets.chomp.to_i
-  first_number
-end
+def randomizer
+  puts 'Would you like to [randomize] the Tomes?'
 
-def AA
-  puts 'first number'
-  first_number = gets.chomp.to_i
-  first_number
-end
+  operator = gets.chomp
 
-def AA
-  puts 'first number'
-  first_number = gets.chomp.to_i
-  first_number
-end
+  if operator == 'randomize'
+    answer = 
 
-def AA
-  puts 'first number'
-  first_number = gets.chomp.to_i
-  first_number
-end
-
-def AA
-  puts 'first number'
-  first_number = gets.chomp.to_i
-  first_number
-end
-
-def AA
-  puts 'first number'
-  first_number = gets.chomp.to_i
-  first_number
-end
-
-def AA
-  puts 'first number'
-  first_number = gets.chomp.to_i
-  first_number
-end
-
-def AA
-  puts 'first number'
-  first_number = gets.chomp.to_i
-  first_number
-end
-
-
-def BB
-  puts 'bright beacon'
-  second_number = gets.chomp.to_i
-  second_number
-end
 
 def question
-puts 'Would you like to [add] or [subtract], [multiply] or [divide]?
+puts 'Would you like to [add] or [subtract], [multiply] or [divide]?'
 
 operator = gets.chomp
 
--- ToA.rb
+++ ToA.rb
@@ -33,6 +33,14 @@
 # Tough Decisions to make, 
 # Option 1 = Use Extant_Exception as the Overflow of words that are NOT Abbreviation Aligned ex. A.A.
 # Option 2 = Make a seperate Misc File that Holds the Overflow NOT A.A. (Abbreviation Aligned) ex. Old_Trends
+
+
+myArray = ["AA", "BB", "CC", "DD", "EE", "FF", "GG", "HH", "II", "JJ", "KK", "LL", "MM", "NN", "OO", "PP", "QQ", "RR", "SS", "TT", "UU", "VV", "WW", "XX", "YY", "ZZ"]
+item = myArray[rand(myArray.length)]
+
+toaArray = ["Alluring_Aura", "Bright_Beacon", "Cosmic_Concoction", "Dim_Dawn", "Elemental_Enchantment", "Frequent_Form", "Golden_Grove", "Hollow_Hum", "Invisioning_Impact", "Jades_in_Jungles", "Keen_Kindness", "Lucid_Levitation", "Mellow_Mantra", "Number_Nine", "Own_my_Omen", "Polar_Purge", "Quantum_Quake", "Relaxing_Rhythm", "Sunshade_Sensei", "Tilting_Tree", "Usual_Underdog", "Vivid_Visions", "Wind_on_Water", "Extant_Exception", "Yuletide_Yogi", "Zen_Zeal"]
+toaitem = toaArray[rand(toaArray.length)]
+
 AA = "Alluring_Aura"
 BB = "Bright_Beacon"
 CC = "Cosmic_Concoction"
@@ -43,19 +51,29 @@ GG = "Golden_Grove"
 HH = "Hollow_Hum"
 II = "Invisioning_Impact"
 JJ = "Jades_in_Jungles"
-KK = "Keen_Kindness"
+KK = "Keen_Kindness",
 LL = "Lucid_Levitation"
 MM = "Mellow_Mantra"
-NN = "New_Nights"
-OO = "Own-my_Omen"
+NN = "Number_Nine"
+OO = "Own_my_Omen"
 PP = "Polar_Purge"
 QQ = "Quantum_Quake"
 RR = "Relaxing_Rhythm"
 SS = "Sunshade_Sensei"
-TT = "Thickening_Transcendence" + "Tilting_Tree"
+TT = "Tilting_Tree"
 UU = "Usual_Underdog"
 VV = "Vivid_Visions"
 WW = "Wind_on_Water"
-XX = "Extant_Exception" + ""
-YY = ""
-ZZ = ""
+XX = "Extant_Exception"
+YY = "Yuletide_Yogi"
+ZZ = "Zen_Zeal"
+
+
+
+
+
+
+
+
+
+
